7d9fe687810cb81293917c0a586a76efba1190a4
jahanzaib5244 <zaibmsf333@gmail.com>
2021-04-22 05:46:11 +0500

    my first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header/header';
+import '../node_modules/materialize-css/dist/css/materialize.min.css';
+
+function App() {
+  return (
+    
+    <div>
+    <Header />  
+      
+    </div>
+  );
+}
+
+export default App;
